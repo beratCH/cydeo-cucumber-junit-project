8f4d6f7465f9c6f2b5646be077ad59380f89ce46
beratCH <beroloji@gmail.com>
2024-04-05 18:47:38 +0200

    ll

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -2,12 +2,11 @@ package com.cydeo.step_definitions;
 
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-public class Hook {
+public class Hooks {
 
     //@Before (value = "@login", order = 2)
     public void setupMethod() {
--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -7,33 +7,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Driver {
 
     //create a private constructor to remove access to this object
-    private Driver(){}
+    private Driver() {
+    }
 
     /*
     We make the WebDriver private, because we want to close access from outside the class.
     We are making it static, because we will use it in a static method.
      */
-  //  private static WebDriver driver; // default value = null
-private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+    //  private static WebDriver driver; // default value = null
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
     /*
     Create a re-usable utility method which will return the same driver instance once we call it.
     - If an instance doesn't exist, it will create first, and then it will always return same instance.
      */
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if(driverPool.get() == null){
+        if (driverPool.get() == null) {
 
-            /*
-            We will read our browserType from configuration.properties file.
-            This way, we can control which browser is opened from outside our code.
-             */
             String browserType = ConfigurationReader.getProperty("browser");
 
             /*
             Depending on the browserType returned from the configuration.properties
             switch statement will determine the "case", and open the matching browser.
              */
-            switch (browserType){
+            switch (browserType) {
                 case "chrome":
                     // WebDriverManager.chromedriver().setup();
                     driverPool.set(new ChromeDriver());
@@ -43,8 +41,8 @@ private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThr
                 case "firefox":
                     // WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
-                driverPool.get().manage().window().maximize();
-                    // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    driverPool.get().manage().window().maximize();
+                    //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
             }
 
@@ -57,8 +55,8 @@ private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThr
     /*
     Create a new Driver.closeDriver(); it will use .quit() method to quit browsers, and then set the driver value back to null.
      */
-    public static void closeDriver(){
-        if (driverPool.get()!=null){
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
             /*
             This line will terminate the currently existing driver completely. It will not exist going forward.
              */
